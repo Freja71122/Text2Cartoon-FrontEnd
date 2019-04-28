2dcd59d1d1a5817ead8ca136e2b0939e0f1a4d58
Ann <1552610@tongji.edu.cn>
2019-04-28 13:50:49 +0800

    first commit

---
+++ js/text.js
@@ -0,0 +1,78 @@
+// set var and return input value
+function getInput() {
+  command = commandInput.value;
+  return command;
+}
+
+// construct the html+text of the input command
+function attachCommand() {
+  // the command you entered
+  var li = document.createElement('li');
+  li.className = 'add_info'
+  li.textContent = getInput();
+  
+  // the path
+  var span = document.createElement('span');
+  span.className = 'path';
+  span.textContent = path;
+  
+  // attach path before command
+  li.insertBefore(span,li.firstChild);
+  
+  // and attach to window
+  result.appendChild(li);
+}
+
+// construct the html+text of the response
+function returnResponse() {
+  var li = document.createElement('li');
+  
+  // plain command vs starting with sudo
+  var answer = (getInput().indexOf('sudo') === 0) ? 'Nice try. No.' : 'Rendering ...';
+  if (getInput().indexOf('$clear$') === 0){
+    var lis = result.getElementsByClassName('add_info')
+    for(var i=lis.length;i>0;i--){
+      result.removeChild(lis[i-1]);
+    }
+  }
+  else{
+    li.textContent = answer;
+    li.className = 'add_info'
+    // and attach to window
+    result.appendChild(li);
+  }
+}
+
+// set input to last command on up key (see event listener below)
+function repeatInput() {
+  commandInput.value = command;
+}
+
+// scroll to bottom...
+function scrollToBottom() {
+  result.scrollTop = result.scrollHeight;
+}
+
+// run all functions on enter (see event listener below)
+function doTheThing() {
+  if(getInput().trim() !== '') { // trim whitespace and check if empty
+    attachCommand();
+    returnResponse();
+    scrollToBottom();
+  }
+  
+  commandInput.value = ''; // clear input value 
+}
+
+// var hoisting transports this to the top
+var commandInput = document.getElementById('command-input'),
+    result = document.querySelector('.result'),
+    command = '',
+    // path = 'c:\\scrivener\\bartleby\\> ';
+    path = 'xx> ';
+
+// whenever you press a key
+commandInput.addEventListener('keydown',function(e){
+  if(e.keyCode === 13) doTheThing(); // enter key
+  if(e.keyCode === 38) repeatInput(); // up key
+});
